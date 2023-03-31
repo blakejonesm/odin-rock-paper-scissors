beec3fbfe1e7ea9a7b7da8016974fde570969ab9
blake <97263263+blakejonesm@users.noreply.github.com>
2023-03-31 15:39:59 +1100

    Implement function for computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+
+    if (randomNumber == 0) {
+        cpuChoice = "rock";
+    } else if (randomNumber == 1) {
+        cpuChoice = "paper";
+    } else {
+        cpuChoice = "scissors";
+    }
+    console.log(cpuChoice);
+    return cpuChoice;
+}
+
+
+
+getComputerChoice()
